716f583b29eac1b553a89eecd3d21a7a71bf86be
Bernardo <bernaferrari2@gmail.com>
2020-10-26 17:56:01 -0300

    Add a test

---
+++ test/color_bloc_test.dart
@@ -0,0 +1,17 @@
+import 'package:fastdarktheme/example/blocs/color_cubit.dart';
+import 'package:fastdarktheme/example/blocs/mode.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('CounterBloc', () {
+    ColorCubit colorBloc;
+
+    setUp(() {
+      colorBloc = ColorCubit();
+    });
+
+    test('initial state', () {
+      expect(colorBloc.state.mode, Mode.WhatsApp);
+    });
+  });
+}
